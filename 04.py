5a631122a3ef77e0aeb30655638f4388587dec36
Maira <mairatariq896@gmail.com>
2024-09-05 10:51:13 -0700

    add files

---
+++ 04.py
@@ -0,0 +1,42 @@
+userName:str ="'maira' tariq"
+userName:str ='"maira" tariq'
+userName:str = '''Maira 
+Tariq'''
+# 
+username:str = "maira"
+userProfession :str = "software engineer"
+userprogrammerlanguage :str = "python"
+userBio:str = username + ' is a ' + userProfession , 'she is learning ' + userprogrammerlanguage
+print(userBio)
+# 
+Name:str = "samra"
+Profession :str = "engineer"
+programmerlanguage :str = "python"
+userBio:str = (f"{(Name)} is a {(Profession)} she is learning {(programmerlanguage)}")
+print(userBio)
+# line execute on another line (function tool) 
+Name : str = "mariam"
+print(Name)
+def addtwonumbers(): 
+ num1:int = 20
+ num2:int = 20
+ result:int = num1 + num2
+ print(result)
+Profession : str = "software engineer"
+print(Profession)
+addtwonumbers()
+language : str = "python "
+print(language)  
+#  when small block start tu is  k uper wali line may colon : aye ga 
+ #  when small block end tu is  k down wali line may no colon : aye ga hai
+#  local block
+def addtwonumbers(): 
+ num1:int = 20
+ num2:int = 20
+ result:int = num1 + num2 
+ print(result)
+  
+addtwonumbers(60 ,10)
+addtwonumbers(50 ,20)
+
+
